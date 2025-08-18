7ac70f74b22374cc3c3e6def37f84c3e247fcda1
MuratGuelr <murat134714@gmail.com>
2025-08-18 17:25:18 +0300

    first commit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCqMRYutVVG2uA8vus_qPOHmJ4lcj88zOc",
-  authDomain: "termino-88e3f.firebaseapp.com",
-  projectId: "termino-88e3f",
-  storageBucket: "termino-88e3f.firebasestorage.app",
-  messagingSenderId: "478107821303",
-  appId: "1:478107821303:web:52a94ffc65454eef551ff2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
